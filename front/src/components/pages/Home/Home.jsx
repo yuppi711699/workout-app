7b037dffa37bbc0f16bd240966e426f7a1151495
Di <di@Dis-MacBook-Pro.local>
2022-11-17 21:16:55 +0100

    scratch front

---
+++ front/src/components/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../common/Header/Header.jsx'
+import styles from './Home.module.scss'
+
+const Home = () => {
+	return (
+		<>
+			<Header />
+			<div>Home page</div>
+		</>
+	)
+}
+
+export default Home
